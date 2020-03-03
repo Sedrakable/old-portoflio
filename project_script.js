d48d750a9149d819f0284a6c9329881cd0968661
Sedrakable <sedrak005@gmail.com>
2020-03-03 17:09:14 -0500

    inner arrows done

--- project_script.js
+++ project_script.js
@@ -6,7 +6,10 @@ const Items = document.querySelectorAll('.item_wrapper');
                     item.childNodes[3].classList.remove('img_darken');
                     item.childNodes[3].classList.add('img_ligthen');  
                     item.childNodes[5].classList.remove('reveal_text');
-                    item.childNodes[5].classList.add('hide_text');      
+                    item.childNodes[5].classList.add('hide_text'); 
+                    item.childNodes[9].classList.remove('reveal_dot');  
+                    item.childNodes[9].classList.add('hide_dot');      
+
                 })
 
                 item.addEventListener('mouseover', () => {
@@ -14,6 +17,8 @@ const Items = document.querySelectorAll('.item_wrapper');
                     item.childNodes[3].classList.add('img_darken');
                     item.childNodes[5].classList.remove('hide_text');
                     item.childNodes[5].classList.add('reveal_text');
+                    item.childNodes[9].classList.remove('hide_dot');  
+                    item.childNodes[9].classList.add('reveal_dot'); 
 
                 })
         })
@@ -24,8 +29,11 @@ const closePopupButton = document.querySelectorAll('.close_button')
 const overlay = document.getElementById('overlay')
 const leftButton = document.getElementById('left_arrow')
 const rightButton = document.getElementById('right_arrow')
+const leftFleshButton = document.getElementById('left_flesh')
+const rightFleshButton = document.getElementById('right_flesh')
 
 var currentCategory;
+var currentBackground;
 
 
 function Open(button){
@@ -97,6 +105,20 @@ function Replace(button){
     popupTitleContainer.replaceChild(cloneTitle, popupTitle);
     popupDescriptionContainer.replaceChild(cloneDescription, popupDescription);
     tryer()
+    fleshTryer()
+}
+
+function innerReplace(button){
+
+    const popup = document.getElementById('popup')
+
+    const popupBackgroundContainer = popup.childNodes[1].childNodes[1];
+    const popupBackground = popup.childNodes[1].childNodes[1].childNodes[1];
+    const background = button;
+    const cloneBackground = background.cloneNode(true);
+
+    popupBackgroundContainer.replaceChild(cloneBackground, popupBackground);
+
 }
 
 
@@ -104,7 +126,9 @@ openPopupButton.forEach(button => {
     button.addEventListener('click', () => {  
         Open(button)    
         currentCategory = button;
+        currentBackground = currentCategory.childNodes[1].childNodes[1];
         tryer()
+        fleshTryer()
     })
 })
 
@@ -151,6 +175,28 @@ function closePopup(popup) {
         rightClick()       
     })
 
+    function leftFleshClick(){       
+        const leftCategory = currentBackground.previousSibling.previousSibling; 
+        currentBackground = leftCategory;     
+        innerReplace(leftCategory)
+        fleshTryer()
+    }
+
+    function rightFleshClick(){
+        const rightCategory = currentBackground.nextSibling.nextSibling;    
+        currentBackground = rightCategory;  
+        innerReplace(rightCategory)
+        fleshTryer()
+    }
+
+    leftFleshButton.addEventListener('click', () => {  
+        leftFleshClick()   
+    })
+
+    rightFleshButton.addEventListener('click', () => {  
+        rightFleshClick()   
+    })
+
     function tryer(){
         const rightCategory = currentCategory.nextSibling.nextSibling;
         
@@ -176,3 +222,56 @@ function closePopup(popup) {
         
     }
 
+    function fleshTryer(){
+        const rightBackground = currentBackground.nextSibling.nextSibling;
+        
+
+        if(rightBackground == null)
+        {
+            rightFleshButton.classList.add('blocked')
+        }
+        else{
+            rightFleshButton.classList.remove('blocked')
+        }
+        
+
+        const leftBackground = currentBackground.previousSibling.previousSibling;
+
+        if(leftBackground == null)
+        {
+            leftFleshButton.classList.add('blocked')
+        }
+        else{
+            leftFleshButton.classList.remove('blocked')
+        }
+        
+    }
+
+    function dotChecker(){
+        //need to go throgh all picitures and find out the right number of pitures 
+        const item_grid = document.getElementById('item_grid');
+        const dot = document.getElementById('dots_holder').childNodes[1];
+        console.log(dot) 
+        
+        const x = item_grid.childElementCount;   
+        
+        for(let i = 1; i <= x; i++){
+            const n = (2*i) -1;
+            const dotContainer = item_grid.childNodes[n].childNodes[9];       
+            const y = item_grid.childNodes[n].childNodes[1].childElementCount;
+            const dotClones = [dot.cloneNode(true)];
+            
+            
+                for(let i = 0; i < y; i++){
+                    
+                    
+                    
+                    dotContainer.appendChild(dotClones[i]); 
+                    dotClones.push(dot.cloneNode(true))     
+                }
+            }    
+    }
+
+    window.onload = function() {
+        dotChecker();
+      };
