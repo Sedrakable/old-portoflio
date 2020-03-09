6f3ba40579dc4292054b091cc3225add868d0f7b
Sedrakable <sedrak005@gmail.com>
2020-03-08 21:36:24 -0400

    index logo

--- switcher.js
+++ switcher.js
@@ -1,7 +1,7 @@
 
-var myStringArray = ["&lt;|","&lt;&nbsp;","&lt;C|","&lt;C&nbsp;","&lt;Co|","&lt;Co&nbsp;","&lt;Cod|","&lt;Cod&nbsp;","&lt;Code|","&lt;Code&nbsp;","&lt;Code|&gt;","&lt;Code&nbsp;&gt;"];
-myStringArray.push("&lt;|","&lt;&nbsp;","&lt;D|","&lt;D&nbsp;","&lt;Dr|","&lt;Dr&nbsp;","&lt;Dra|","&lt;Dra&nbsp;","&lt;Draw|","&lt;Draw&nbsp;","&lt;Draw|&gt;","&lt;Draw&nbsp;&gt;");
-myStringArray.push("&lt;|","&lt;&nbsp;","&lt;P|","&lt;P&nbsp;","&lt;Pa|","&lt;Pa&nbsp;","&lt;Pai|","&lt;Pai&nbsp;","&lt;Pain|","&lt;Pain&nbsp;","&lt;Paint|","&lt;Paint&nbsp;","&lt;Paint|&gt;","&lt;Paint&nbsp;&gt;");
+var myStringArray = ["&lt;|","&lt;&nbsp;","&lt;C|","&lt;C&nbsp;","&lt;Co|","&lt;Co&nbsp;","&lt;Cod|","&lt;Cod&nbsp;","&lt;Code|","&lt;Code&nbsp;","&lt;Code|&gt;","&lt;Code&nbsp;&gt;","&lt;Code&gt;|"];
+myStringArray.push("&lt;|","&lt;&nbsp;","&lt;D|","&lt;D&nbsp;","&lt;Dr|","&lt;Dr&nbsp;","&lt;Dra|","&lt;Dra&nbsp;","&lt;Draw|","&lt;Draw&nbsp;","&lt;Draw|&gt;","&lt;Draw&nbsp;&gt;","&lt;Draw&gt;|");
+myStringArray.push("&lt;|","&lt;&nbsp;","&lt;P|","&lt;P&nbsp;","&lt;Pa|","&lt;Pa&nbsp;","&lt;Pai|","&lt;Pai&nbsp;","&lt;Pain|","&lt;Pain&nbsp;","&lt;Paint|","&lt;Paint&nbsp;","&lt;Paint|&gt;","&lt;Paint&nbsp;&gt;","&lt;Paint&gt;|");
 
 var i =0;
 // const
