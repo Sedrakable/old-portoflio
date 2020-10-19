caf3a7d9e84b6236071a4b0432fd27b6ac1013bd
Sedrakable <sedrak005@gmail.com>
2020-10-19 16:19:13 -0400

    fixed about

--- switcher.js
+++ switcher.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var caregoryWords = ["Web Developer","Game Developer","Digital Artist","Origami Artist","Painter","Calithenict Athlete"];
 var myStringArray = [];
 var switchSpeed = 180;
@@ -55,64 +55,4 @@ window.onload = function() {
       
   stringCreator();
 };
-
-=======
-var caregoryWords = ["Web Developer","Game Developer","Digital Artist","Origami Artist","Painter","Calithenict Athlete"];
-var myStringArray = [];
-var switchSpeed = 180;
-
-
-function stringCreator(){
-  
-  for (let n = 0; n < caregoryWords.length; n++) {
-    var currentWord = "";
-    myStringArray.push("&nbsp");
-    myStringArray.push("|");
-    myStringArray.push("&lt;" + "&nbsp");
-    myStringArray.push("&lt;" + "|");
-    
-    for (let m = 0; m < caregoryWords[n].length; m++) {
-        
-        currentWord = currentWord + caregoryWords[n].charAt(m);
-
-        myStringArray.push("&lt;" + currentWord + "|" )  
-       
-        myStringArray.push("&lt;" + currentWord + "&nbsp")  
-
-        
-    }
-    myStringArray.push("&lt;" + currentWord + "&gt;" + "&nbsp");
-    myStringArray.push("&lt;" + currentWord + "&gt;"+"|");
-    myStringArray.push("&lt;" + currentWord + "&gt;" + "&nbsp");
-    
-    
-  }
-  console.log(myStringArray)
-}
-
-var i = 0;
-
-function switchee(){
-    
-  const word = document.getElementById('word')
-
-  word.innerHTML = myStringArray[i];
-  i++
-  if(i == myStringArray.length ){
-    i = 0
-  }
-  
-}
-
-window.setInterval(function(){
-    switchee()
-    
-  }, switchSpeed);
-
-window.onload = function() {
-      
-  stringCreator();
-};
-
->>>>>>> 053d2b5e3db00e2c8d4ad9db05c262f753bb53f4
   
