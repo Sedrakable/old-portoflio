ec02a237d7f4b4b96a635c493ae12ee113cc290b
Sedrak <43288178+Sedrakable@users.noreply.github.com>
2020-11-04 13:19:04 -0500

    unity

--- my_parallax_v2.js
+++ my_parallax_v2.js
@@ -1,31 +1,40 @@
 var offsetY = 0;
 const offsetMultiplier = 0.4;
 var background = document.getElementById("background");
-
-if (background === null) {
-  console.log("reee");
-  background = document.getElementById("project_background");
-  window.addEventListener(
-    "scroll",
-    function (e) {
-      offsetY = window.pageYOffset;
-      scrollSpeed();
-    },
-    false
-  );
-}
 var height = background.offsetHeight;
+// if (background === null) {
+//   console.log("reee");
+//   background = document.getElementById("project_background");
+//   window.addEventListener(
+//     "scroll",
+//     function (e) {
+//       offsetY = window.pageYOffset;
+//       scrollSpeed();
+//     },
+//     false
+//   );
+// }
+
+window.addEventListener(
+  "scroll",
+  function (e) {
+    offsetY = window.pageYOffset;
+    scrollSpeed();
+  },
+  false
+);
+
 // var itemsAndSpeed = [[document.getElementById('background'),0.5]];
 
-// window.addEventListener(
-//   "resize",
-//   function (e) {
-//
-//     height = background.offsetHeight;
-//     scrollSpeed();
-//   },
-//   false
-// );
+window.addEventListener(
+  "resize",
+  function (e) {
+    console.log("resised");
+    height = background.offsetHeight;
+    scrollSpeed();
+  },
+  false
+);
 
 function scrollSpeed() {
   var x = offsetMultiplier * offsetY;
